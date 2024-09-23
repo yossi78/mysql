ecf95e7624c3099f8cc1aed221866be9dc2c747d
yossi78 <yossi78@gmail.com>
2024-09-23 12:36:59 +0300

    Add business logic

---
+++ src/main/java/com/example/mysql/api/UserController.java
@@ -0,0 +1,62 @@
+package com.example.mysql.api;
+import com.example.mysql.data_layer.User;
+import com.example.mysql.exception.ResourceNotFoundException;
+import com.example.mysql.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+@RestController
+@RequestMapping("/api/users")
+@Slf4j
+public class UserController {
+    private final UserService userService;
+
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+
+
+    @GetMapping(path = "/{userId}")
+    public ResponseEntity<User> findUserById(@PathVariable("userId") Long userId) {
+        try {
+            User user = userService.getUser(userId);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            log.error("Failed to get user from DB ");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
+        return userService.updateUser(userId, updatedUser);
+    }
+
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+    }
+
+
+}
---
+++ src/main/java/com/example/mysql/service/UserService.java
@@ -0,0 +1,59 @@
+package com.example.mysql.service;
+
+
+import com.example.mysql.data_layer.User;
+import com.example.mysql.data_layer.UserRepository;
+import com.example.mysql.exception.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@Slf4j
+public class UserService {
+
+
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public User getUser(Long userId) {
+        User user =  userRepository.findById(String.valueOf(userId)).orElse(null);
+        if(user==null){
+            log.error("The user has not been found , userId="+userId);
+            throw new ResourceNotFoundException("The user has not been found");
+        }
+        log.info("Find user by id: " + userId);
+        return user;
+    }
+
+
+    public User updateUser(Long userId, User updatedUser) {
+        if (userRepository.existsById(String.valueOf(userId))) {
+            updatedUser.setId(userId);
+            return userRepository.save(updatedUser);
+        }
+        return null;
+    }
+
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+
+
+
+}
