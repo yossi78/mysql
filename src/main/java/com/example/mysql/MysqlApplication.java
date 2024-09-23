51687c48f87dec4933ec94a815a3e74df172674e
yossi78 <yossi78@gmail.com>
2024-09-23 11:51:16 +0300

    First commit

---
+++ src/main/java/com/example/mysql/MysqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlApplication {
+
+	// test
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlApplication.class, args);
+	}
+
+}
