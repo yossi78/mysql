21170d24a1b3a8ff43b42ec66abf0f39c95cebcc
yossi78 <yossi78@gmail.com>
2024-10-20 17:13:50 +0300

    update bl

--- src/main/java/com/example/mysql/service/UserService.java
+++ src/main/java/com/example/mysql/service/UserService.java
@@ -1,13 +1,10 @@
 package com.example.mysql.service;
-
-
 import com.example.mysql.data_layer.User;
 import com.example.mysql.data_layer.UserRepository;
 import com.example.mysql.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 
@@ -58,8 +55,5 @@ public class UserService {
     public void deleteUser(String userId) {
         userRepository.deleteById(userId);
     }
-
-
-
 
 }
